65f711ebfce737598c6eebcb49e014d63c491a07
Robert Szulist <r.szulist@gmail.com>
2017-11-14 23:51:28 +0100

    Add basic parameters

--- zappix/sender.py
+++ zappix/sender.py
@@ -4,6 +4,7 @@ Python implementation of Zabbix sender.
 
 from __future__ import print_function, unicode_literals, absolute_import
 import socket
+import argparse
 
 
 class sender(object):
@@ -21,3 +22,14 @@ class sender(object):
             print("Cannot connect to host.")
         finally:
             s.close()
+
+
+if __name__ == '__main__':
+    params = argparse.ArgumentParser()
+    params.add_argument('-z', '--zabbix', nargs=1)
+    params.add_argument('-p', '--port', nargs='?', default=10051, type=int)
+    params.add_argument('-s', '--host', action="store_true")
+    params.add_argument('-k', '--key', action="store_true")
+    params.add_argument('-o', '--value', action="store_true")
+    args = params.parse_args()
+
