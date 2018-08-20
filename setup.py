699aa0c803ed3a23bb67e233be2c62d4d9a69323
Robert Szulist <r.szulist@gmail.com>
2018-08-20 09:30:11 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+Project home:
+https://github.com/szuro/zappix
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='zappix',
+    version='0.0.1',
+    description='Package for polarization state calculations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/szuro/zappix',
+    author='Robert Szulist',
+    author_email='r.szulist@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='zabbix get sender',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['numpy'],
+)
