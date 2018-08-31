08dfe40baccb3594488db9ddd1548e06291dfb50
Robert Szulist <r.szulist@gmail.com>
2018-08-31 22:11:38 +0200

    Refactor code

--- zappix/sender.py
+++ zappix/sender.py
@@ -6,24 +6,42 @@ from __future__ import print_function, unicode_literals, absolute_import
 import socket
 import argparse
 import json
+from copy import deepcopy
 
 
 class sender(object):
+
+    _container = {"request": "sender data", "data": []}
+
     def __init__(self, ip, port=10051):
         self.ip = ip
         self.port = port
 
-    def send(self, payload):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def send_value(self, host, key, value):
+        payload = {
+            "host": host,
+            "key": key,
+            "value": value
+        }
+
+        full_crate = json.dumps(
+                self._pack_payload(payload)
+            ).encode('utf_8')
+        return self._send(full_crate)
+
+    def _pack_payload(self, payload):
+        crate = deepcopy(self._container)
+        crate['data'].append(payload)
+        return crate
+
+    def _send(self, payload):
+        data = b""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.ip, self.port))
             s.send(payload)
             data = s.recv(124)
-        except socket.error:
-            print("Cannot connect to host.")
-        finally:
-            s.close()
-    
+            return data.decode("utf_8")
+
     def create_single_request(self, host, key, value):
         container = {"request": "sender data", "data": []}
         payload = {"host": host, "key": key, "value": value}
@@ -42,5 +60,4 @@ if __name__ == '__main__':
     args = params.parse_args()
 
     zab = sender(args.zabbix, args.port)
-    zab.send(zab.create_single_request(args.host, args.key, args.value))
-
+    zab.send_value(args.host, args.key, args.value)
