6953fcaaba26abde1dc78ccb3ce11e383d0cfffa
Robert Szulist <r.szulist@gmail.com>
2018-09-16 21:21:32 +0200

    Add basic Sender tests

---
+++ tests/test_Sender.py
@@ -0,0 +1,58 @@
+import unittest
+import configparser
+import os
+from zappix.sender import Sender
+
+
+class SenderValueTest(unittest.TestCase):
+    def setUp(self):
+        config = configparser.ConfigParser()
+        this_path = os.path.dirname(os.path.abspath(__file__))
+        config.read(os.path.join(this_path, 'test.ini'))
+        self.server = config['server']['good']
+
+        self.sender = Sender(self.server)
+
+
+    def test_single_value(self):
+        resp = self.sender.send_value('testhost','test', 1)
+        self.assertIsNotNone(resp)
+        self.assertIsNotNone(resp['info'])
+        self.assertRegex(resp['info'], r"processed: 1; failed: 0; total: 1; seconds spent: [0-9]+.[0-9]+")
+
+
+    def test_bad_value(self):
+        resp = self.sender.send_value('testhost','test', "bad_value")
+        self.assertIsNotNone(resp)
+        self.assertIsNotNone(resp['info'])
+        self.assertRegex(resp['info'], r"processed: 0; failed: 1; total: 1; seconds spent: [0-9]+.[0-9]+")
+
+
+    def test_bad_key(self):
+        resp = self.sender.send_value('testhost','bad_key', 1)
+        self.assertIsNotNone(resp)
+        self.assertIsNotNone(resp['info'])
+        self.assertRegex(resp['info'], r"processed: 0; failed: 1; total: 1; seconds spent: [0-9]+.[0-9]+")
+
+
+    def test_bad_hostname(self):
+        resp = self.sender.send_value('bad_host','test', 1)
+        self.assertIsNotNone(resp)
+        self.assertIsNotNone(resp['info'])
+        self.assertRegex(resp['info'], r"processed: 0; failed: 1; total: 1; seconds spent: [0-9]+.[0-9]+")
+
+
+    @unittest.skip
+    def test_bad_server(self):
+        sender = Sender('127.0.0.2')
+        resp = sender.send_value('testhost','test', 1)
+        self.assertIsNotNone(resp)
+        self.assertIsNotNone(resp['info'])
+
+
+    @unittest.skip
+    def test_bad_port(self):
+        sender = Sender(self.server, 666)
+        resp = sender.send_value('testhost','test', 1)
+        self.assertIsNotNone(resp)
+        self.assertIsNotNone(resp['info'])
