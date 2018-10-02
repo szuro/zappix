77e0498b23897b9e48357a066fca5cf78a871f8e
Robert Szulist <r.szulist@gmail.com>
2018-10-02 20:49:21 +0200

    Use temporaty files in sender tests
    Also send a set number of values, so the regex can be more precise.

--- tests/test_Sender.py
+++ tests/test_Sender.py
@@ -1,6 +1,7 @@
 import unittest
 import configparser
 import os
+import time
 import tempfile
 from zappix.sender import Sender
 
@@ -55,18 +56,36 @@ class SenderFileTest(unittest.TestCase):
         self.server = config['server']['good']
 
         self.sender = Sender(self.server)
-        self.file = os.path.join(this_path, 'test.csv')
+        
+        self.file = tempfile.NamedTemporaryFile('w+', delete=False)
+        self.file.write("testhost test 1\n"
+                        "testhost test 2\n"
+                        "testhost test 3\n")
+        self.file.close()
+
+        self.file_with_timestamps = tempfile.NamedTemporaryFile('w+', delete=False)
+        self.file_with_timestamps.write("testhost test {t} 10\n"
+                                        "testhost test {t} 20\n"
+                                        "testhost test {t} 30\n".format(t=int(time.time()//1)))
+        self.file_with_timestamps.close()
+
+
+    def tearDown(self):
+        os.unlink(self.file.name)
+        os.unlink(self.file_with_timestamps.name)
 
     def test_send_file(self):
-        resp = self.sender.send_file(self.file)
+        resp = self.sender.send_file(self.file.name)
         self.assertIsNotNone(resp)
-        self.assertRegex(resp, r"processed: [0-9]+; failed: 0; total: [0-9]+; seconds spent: [0-9]+.[0-9]+")
+        self.assertRegex(resp, r"processed: 3; failed: 0; total: 3; seconds spent: [0-9]+.[0-9]+")
 
     def test_send_corrupted_file(self):
         pass
 
     def test_send_file_with_timestamps(self):
-        pass
+        resp = self.sender.send_file(self.file_with_timestamps.name, with_timestamps=True)
+        self.assertIsNotNone(resp)
+        self.assertRegex(resp, r"processed: 3; failed: 0; total: 3; seconds spent: [0-9]+.[0-9]+")
 
     def test_send_corrupted_file_with_timestamps(self):
         pass
