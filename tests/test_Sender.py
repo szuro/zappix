d0f00f834ad69416fc411b01547da1ca00571330
Robert Szulist <r.szulist@gmail.com>
2019-03-09 23:35:11 +0100

    Add file send mock test

--- tests/test_Sender.py
+++ tests/test_Sender.py
@@ -1,4 +1,6 @@
 import unittest
+import tempfile
+import os
 from unittest.mock import patch, MagicMock
 from zappix.sender import Sender
 
@@ -28,3 +30,22 @@ class TestSender(unittest.TestCase):
 
         self.assertIsNotNone(result.pop("seconds spent"))
         self.assertDictEqual(result, {"processed": 1, "failed": 0, "total": 1})
+
+    @patch('zappix.dstream.socket')
+    def test_send_file(self, mock_socket):
+        file_ = tempfile.NamedTemporaryFile('w+', delete=False)
+        file_.write("testhost test 1\n"
+                    "testhost test  2\n"
+                    "testhost test   3\n")
+        file_.close()
+        mock_socket.create_connection.return_value = self.msock
+        self.msock.recv.side_effect = [
+            b'ZBXD\x01', b'\x5b\x00\x00\x00\x00\x00\x00\x00',
+            b'{"response":"success", "info":"processed: 3; failed: 0; total: 3; seconds spent: 0.060753"}', b''
+            ]
+
+        sender = Sender('localhost')
+        resp, _ = sender.send_file(file_.name)
+        os.unlink(file_.name)
+        self.assertIsNotNone(resp.pop("seconds spent"))
+        self.assertDictEqual(resp, {"processed": 3, "failed": 0, "total": 3})
