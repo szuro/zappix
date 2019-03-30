b62541393cf659351928e3e3378754b94eef3b3a
Robert Szulist <r.szulist@gmail.com>
2019-03-30 23:47:52 +0100

    Add ActiveChecksRequest tests

--- tests/test_ActiveChecksRequest.py
+++ tests/test_ActiveChecksRequest.py
@@ -1,2 +1,11 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from ast import literal_eval
+from zappix.protocol import ActiveChecksRequest
+
+
+class TestActiveChecksRequest(unittest.TestCase):
+    def test_basic_init(self):
+        item = ActiveChecksRequest('testhost')
+        item_repr = str(item)
+        d = {"host": "testhost", "request": "active checks"}
+        self.assertDictEqual(literal_eval(item_repr), d)
