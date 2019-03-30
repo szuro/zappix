5abb4bb34b9287e90b43e1789ec478cb442ac6b6
Robert Szulist <r.szulist@gmail.com>
2019-03-30 23:43:26 +0100

    Add AgentData tests

--- tests/test_AgentData.py
+++ tests/test_AgentData.py
@@ -1,2 +1,11 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from ast import literal_eval
+from zappix.protocol import AgentData
+
+
+class TestItemData(unittest.TestCase):
+    def test_basic_init(self):
+        item = AgentData('testhost', 'testkey', 1, 1553980979, 1000)
+        item_repr = str(item)
+        d = {"host": "testhost", "key": "testkey", "value": 1, "clock": 1553980979, "ns": 1000}
+        self.assertDictEqual(literal_eval(item_repr), d)
