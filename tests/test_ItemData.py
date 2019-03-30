0a1507e03c1a38adefef1c70d78f44902ac07104
Robert Szulist <r.szulist@gmail.com>
2019-03-30 22:29:47 +0100

    Add tests for ItemData

--- tests/test_ItemData.py
+++ tests/test_ItemData.py
@@ -1,2 +1,17 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from ast import literal_eval
+from zappix.protocol import ItemData
+
+
+class TestItemData(unittest.TestCase):
+    def test_init_without_clock(self):
+        item = ItemData('testhost', 'testkey', 1)
+        item_repr = str(item)
+        d = {"host": "testhost", "key": "testkey", "value": 1}
+        self.assertDictEqual(literal_eval(item_repr), d)
+
+    def test_init_with_clock(self):
+        item = ItemData('testhost', 'testkey', 1, 1553980979)
+        item_repr = str(item)
+        d = {"host": "testhost", "key": "testkey", "value": 1, "clock": 1553980979}
+        self.assertDictEqual(literal_eval(item_repr), d)
