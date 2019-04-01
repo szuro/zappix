e08474220af16b753f2713e7733dfe257a7516c6
Robert Szulist <r.szulist@gmail.com>
2019-04-01 18:29:21 +0200

    Add tests for AgentDataRequest

--- tests/test_AgentDataRequest.py
+++ tests/test_AgentDataRequest.py
@@ -1,2 +1,57 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from ast import literal_eval
+from zappix.protocol import AgentData, AgentDataRequest
+
+
+class TestAgentDataRequest(unittest.TestCase):
+    def setUp(self):
+        self.empty_agent = {"request": "agent data"}
+        self.agent_with_data = {"request": "agent data", "data": [
+                {"host": "testhost", "key": "testkey", "value": 1, "clock": 1554133179, "ns": 455816800, "id": 1},
+                {"host": "testhost", "key": "testkey2", "value": 10, "clock": 1554133279, "ns": 555816800, "id": 2}
+            ]}
+
+    def test_basic_init(self):
+        agent_request = AgentDataRequest()
+        self.assertIsInstance(agent_request, AgentDataRequest)
+        self.assertDictEqual(
+            literal_eval(str(agent_request)),
+            self.empty_agent
+            )
+
+    def test_init_with_data(self):
+        agent_request = AgentDataRequest(
+            [AgentData('testhost', 'testkey', 1, 1554133179, 455816800),
+             AgentData('testhost', 'testkey2', 10, 1554133279, 555816800)]
+            )
+        self.assertIsInstance(agent_request, AgentDataRequest)
+        self.assertDictEqual(
+            literal_eval(str(agent_request)),
+            self.agent_with_data
+            )
+
+    def test_init_with_invalid_data(self):
+        with self.assertRaises(ValueError):
+            AgentDataRequest([('testhost', 'testkey', 1)])
+
+    def test_add_item(self):
+        agent_request = AgentDataRequest()
+
+        self.assertDictEqual(
+            literal_eval(str(agent_request)),
+            self.empty_agent
+            )
+        agent_request.add_item(AgentData('testhost', 'testkey', 1, 1554133179, 455816800))
+        agent_request.add_item(AgentData('testhost', 'testkey2', 10, 1554133279, 555816800))
+
+        self.assertDictEqual(
+            literal_eval(str(agent_request)),
+            self.agent_with_data
+            )
+
+        self.assertListEqual([ar.id for ar in agent_request.data], [1, 2])
+
+    def test_add_invalid_item(self):
+        with self.assertRaises(ValueError):
+            agent_request = AgentDataRequest()
+            agent_request.add_item(('testhost', 'testkey', 1))
